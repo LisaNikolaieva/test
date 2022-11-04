64b681498643933c3539e2eec44e7e69a3dbb4c1
Yelyzaveta_Nikolaieva <100728186+LisaNikolaieva@users.noreply.github.com>
2022-11-04 09:34:53 +0100

    Add files via upload

--- test2.py
+++ test2.py
@@ -18,43 +18,24 @@ class TesTLineLength(unittest.TestCase):
         a2 = Coord(1, 0)
         self.assertEqual(1, line_length(a1, a2))
 
+
     def test_ut4(self):
-        a1 = Coord(1, 1)
-        a2 = Coord(-3, -2)
-        self.assertEqual(5, line_length(a1, a2))
-
-    def test_ut5(self):
-        a1 = Coord(1, 1)
-        a2 = Coord(0, 1)
-        self.assertEqual(1, line_length(a1, a2))
-
-    def test_ut6(self):
         a1 = Coord(1, 1)
         a2 = Coord(5, 4)
         self.assertEqual(5, line_length(a1, a2))
 
-    def test_ut7(self):
+    def test_ut5(self):
         a1 = Coord(1, 1)
         a2 = Coord(5, -2)
         self.assertEqual(5, line_length(a1, a2))
 
-    def test_ut8(self):
-        a1 = Coord(1, 1)
-        a2 = Coord(4, 5)
-        self.assertEqual(5, line_length(a1, a2))
 
-
-    def test_ut9(self):
+    def test_ut6(self):
         a1 = Coord(1, 1)
         a2 = Coord(-1, 1)
         self.assertEqual(2, line_length(a1, a2))
 
-    def test_ut10(self):
-        a1 = Coord(1, 1)
-        a2 = Coord(1, -1)
-        self.assertEqual(2, line_length(a1, a2))
-
-    def test_ut11(self):
+    def test_ut7(self):
         a1 = Coord(3, 4)
         a2 = Coord(-3, -4)
         self.assertEqual(10, line_length(a1, a2))
